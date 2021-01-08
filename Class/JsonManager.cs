6c4f623c8a1368ddbe6b27c34dc671e35591d213
kosminskimateusz <kosminskimateusz@gmail.com>
2021-01-08 15:50:37 +0100

    Added SerializeToUtf8Bytes to write and read files json

--- Class/JsonManager.cs
+++ Class/JsonManager.cs
@@ -10,29 +10,63 @@ class JsonManager
     {
         People person = new People(name, lastName, age);
         string path = folder + person.Name.ToLower() + person.LastName + ".json";
+        // JsonSerializerOptions ładnie formatuje kod w pliku JSON
         var options = new JsonSerializerOptions
         {
             WriteIndented = true,
         };
-        string jsonString = JsonSerializer.Serialize(person, options);
-        if (!File.Exists($"{folder}{person.Name}{person.LastName}.json"))
+
+        byte[] jsonUtf8Bytes;
+        jsonUtf8Bytes = JsonSerializer.SerializeToUtf8Bytes(person, options);
+        person.LastName = "oko";
+
+        
+
+        if (!File.Exists(path))
         {
-            File.WriteAllText(path, jsonString);
-            Console.WriteLine("Utworzono json.");
+            File.WriteAllBytes(path, jsonUtf8Bytes);
+            Console.WriteLine("Zapisano bajty");
         }
-        else
+        else 
         {
-            Console.WriteLine("Czy nadpisać plik?");
-            string input = "y";
-            if (input == "y")
-            {
-                File.WriteAllText(path, jsonString);
-                Console.WriteLine("Nadpisano plik.");
-            }
-            else
-            {
-                Console.WriteLine("Nie nadpisano pliku");
-            }
+            File.WriteAllBytes(path, jsonUtf8Bytes);
+            Console.WriteLine("Nadpisano bajty");
         }
+
+        var jsonBytes = File.ReadAllBytes(path);
+        var personX = JsonSerializer.Deserialize<People>(jsonBytes);
+
+        Console.WriteLine($"Obiekt z pliku JSON:\n\tName:\t\t{personX.Name}\n\tLast Name:\t{personX.LastName}\n\tAge:\t\t{personX.Age}");
+
+        // string jsonString = JsonSerializer.Serialize(person, options);
+        // if (!File.Exists($"{folder}{person.Name}{person.LastName}.json"))
+        // {
+        //     File.WriteAllText(path, jsonString);
+        //     Console.WriteLine("Utworzono json.");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Czy nadpisać plik?");
+        //     string input = "y";
+        //     if (input == "y")
+        //     {
+        //         File.WriteAllText(path, jsonString);
+        //         Console.WriteLine("Nadpisano plik.");
+        //     }
+        //     else
+        //     {
+        //         Console.WriteLine("Nie nadpisano pliku");
+        //     }
+        // }
+    }
+
+    public void ReadJsonFile(byte[] jsonUtf8Bytes)
+    {
+        var readOnlySpan = new ReadOnlySpan<byte>(jsonUtf8Bytes);
+        var person1 = JsonSerializer.Deserialize<People>(readOnlySpan);
+        Console.WriteLine(person1.LastName);
+        var utf8Reader = new Utf8JsonReader(jsonUtf8Bytes);
+        var person2 = JsonSerializer.Deserialize<People>(ref utf8Reader);
+        Console.WriteLine(person2.LastName);
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,7 +7,12 @@ namespace JsonPractice
         static void Main(string[] args)
         {
             JsonManager manager = new JsonManager();
-            manager.AddOverrideJsonFile("Mateusz", "Kosminski", 29);
+            manager.AddOverrideJsonFile("Mateusz", "Kośmiński", 29);
+            manager.AddOverrideJsonFile("Mariusz", "Kat", 29);
+            manager.AddOverrideJsonFile("Monika", "Kokolino", 29);
+            manager.AddOverrideJsonFile("Seba", "Porter", 29);
+            manager.AddOverrideJsonFile("Michał", "Szpak", 29);
+            manager.AddOverrideJsonFile("Mietek", "Trep", 29);
         }
     }
 }
